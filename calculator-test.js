c992d73244b0f269c22e813a0fac942cd98584dc
jquatjr <91287863+jquatjr@users.noreply.github.com>
2021-10-07 15:01:19 -0400

    my first code

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,37 @@
-
+// Starter code from Springboard
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 100000,
+    years: 30,
+    rate: 12,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('1028.61')
 });
 
 
-it("should return a result with 2 decimal places", function() {
-  // ..
+it("should return a result with 2 decimal places", function () {
+  const values = {
+    amount: 5002,
+    years: 15,
+    rate: 4,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('37.00');
 });
 
-/// etc
+it("should calculate for long terms years", function () {
+  const values = {
+    amount: 50000,
+    years: 99,
+    rate: 5,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('209.84');
+});
+
+it("should calculate for high interest", function () {
+  const values = {
+    amount: 100000,
+    years: 15,
+    rate: 30,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('2529.70');
+});
